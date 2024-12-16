17ca39c4d1e81d94dd6c01144bf85bf49f6f0d77
Roman Sedlář <roman.sedlar.024@pslib.cz>
2024-12-16 10:03:39 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+
+let a = 0
+let b = 1
+
+
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        do 
+        let a = 1
+    }
+    if (a === 1)
+        music.playTone(100, 200)
+    let a = 0
+})
+
